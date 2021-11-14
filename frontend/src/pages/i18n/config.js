07d8a45e6f0a90170cc7128cd75a4bdee6847bc7
Zeyu Gu <mikegu0508@gmail.com>
2021-11-14 00:01:44 -0500

    modified i18n config

--- frontend/src/pages/i18n/config.js
+++ frontend/src/pages/i18n/config.js
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from "react-i18next";
 
 
@@ -8,17 +7,23 @@ import { initReactI18next } from "react-i18next";
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .use(LanguageDetector)
-    .use(HttpApi)
     .init({
+        ns: ['translation'],
+        defaultNS: 'translation',
         supportedLngs: ['en', 'es'],
         fallbackLng: "en",
         detection: {
             order: ['cookie', 'htmlTag', 'localStorage','path', 'subdomain'],
             caches: ['cookie'],
         },
-        backend: {
-            loadPath: '../backend/languages/{{lng}}/translation.json',
-        },
+        resources: {
+            en: {
+                translation: require('../../locales/en/translation.json')
+            },
+            es: {
+                translation: require('../../locales/es/translation.json')
+            }
+        }
     });
 
 export default i18n;
